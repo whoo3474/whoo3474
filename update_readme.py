64520f54487fb181067af53387edf6aea7c8979f
JeongMin Woo <minhan_a@naver.com>
2022-11-21 16:32:05 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -17,9 +17,19 @@ for idx, feed in enumerate(rss_feed['entries']):
     
 markdown_text = """### Hi there 👋
 📬  Contact Email : minhan_a@naver.com
+
 👨🏻‍💻  Tech & Daily Blog : https://minhan2.tistory.com
+
 [![github stats](https://github-readme-stats.vercel.app/api?username=whoo3474&show_icons=true&hide_border=False)](https://minhan2.tistory.com)
+
+
+✨ My Certificate
+
+![image](https://tistory1.daumcdn.net/tistory/2920456/skin/images/hashicorp-certified-terraform-associate.png)![image](https://tistory1.daumcdn.net/tistory/2920456/skin/images/aws-certified-solutions-architect-associate.png)![image](https://tistory1.daumcdn.net/tistory/2920456/skin/images/cka-certified-kubernetes-administrator.png)
+
+
 🤩 Latest Blog Post
+
 """
 
 readme_text = f"{markdown_text}{latest_blog_post_list}"
