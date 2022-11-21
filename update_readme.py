bdb7419df456bd07cf12c33ddcb4fdbd418f7cf1
JeongMin Woo <minhan_a@naver.com>
2022-11-21 16:26:49 +0900

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,28 @@
+import feedparser
+
+minhan_blog_rss_url = "https://minhan2.tistory.com/rss"
+rss_feed = feedparser.parse(minhan_blog_rss_url)
+
+MAX_POST_NUM = 10
+
+latest_blog_post_list = ""
+
+MAX_POST_NUM = 10
+
+for idx, feed in enumerate(rss_feed['entries']):
+    if idx > MAX_POST_NUM:
+        break
+    feed_date = feed['published_parsed']
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+    
+markdown_text = """### Hi there 👋
+📬  Contact Email : minhan_a@naver.com
+👨🏻‍💻  Tech & Daily Blog : https://minhan2.tistory.com
+[![github stats](https://github-readme-stats.vercel.app/api?username=whoo3474&show_icons=true&hide_border=False)](https://minhan2.tistory.com)
+🤩 Latest Blog Post
+"""
+
+readme_text = f"{markdown_text}{latest_blog_post_list}"
+
+with open("README.md", 'w', encoding='utf-8') as f:
+    f.write(readme_text)
